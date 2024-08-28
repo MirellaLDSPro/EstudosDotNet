4b2ed14771134565c0858c48b9a0f6ff3be32cfe
Mirella Lins <mirella.lds@gmail.com>
2024-08-28 11:05:12 -0300

    Substituir texto

---
+++ HelpDoc/HelpDOc/Model/SubstituirModel.cs
@@ -0,0 +1,9 @@
+
+
+public class SubstituirModel {
+    public string? SelectedFile { get; set; }
+    public string? OldText { get; set; }
+    public string? NewText { get; set; }
+    public string? SuccessMessage { get; set; }
+    public string? ErrorMessage { get; set; }
+}
